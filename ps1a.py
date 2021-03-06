6f0924a6c543b00541e66ca56b5e5964ce87bfc1
mgeckil18 <mgeckil18@ku.edu.tr>
2021-03-06 17:00:51 +0000

    Brute force search.

--- ps1a.py
+++ ps1a.py
@@ -3,12 +3,12 @@ T = 0
 # your code here
 i = 1
 formulaTime = 0
-for i in range(n):
+while i <= n :
   if i%2 == 0:
     formulaTime += ((2**i + 1)**(n-i))
   elif i%2== 1 :
     formulaTime += ((3**i + 1)**(n-i))  
-
+  i += 1
 while formulaTime*T <= B:
   T += 1
 # please do not modify the input and print statements
